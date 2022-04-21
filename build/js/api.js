cadf3259c86b31ab664a47c779beb9075d593a76
yodavan <84133554+yodavan@users.noreply.github.com>
2022-04-21 10:23:37 +0100

    Удалил папку build

--- build/js/api.js
+++
@@ -1,1 +0,0 @@
-import{createAviaSegments}from"./creates-cards.js";import{tickets}from"./data.js";import{formList}from"./filter.js";createAviaSegments(tickets);const productButton=document.querySelector(".products__button-list");productButton.addEventListener("change",(()=>{createAviaSegments(tickets)})),formList.addEventListener("change",(()=>{createAviaSegments(tickets)}));
